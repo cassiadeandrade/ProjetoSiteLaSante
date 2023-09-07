6782d423fe3bb6fa59bc5195f928eca81d4dc46f
Cássia de Andrade <cassiaandrade272@gmail.com>
2023-09-07 12:07:14 -0300

    Criação de Site.

---
+++ script.js
@@ -0,0 +1,22 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.classList.add('active');
+        } else {
+            slide.classList.remove('active');
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 5000);
+
+showSlide(currentSlide);
+
